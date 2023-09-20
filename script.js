2f882334249f10fa55ed510f020ca1bfe2097960
Rainers Lovkins <62895879+RainersCode@users.noreply.github.com>
2023-09-20 15:11:26 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,96 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
+
+const music = document.querySelector('audio');
+const prevBtn = document.getElementById('prev');
+const playBtn = document.getElementById('play');
+const nextBtn = document.getElementById('next');
+
+
+//Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army (Remix)',
+        artist: 'Jacinto Design'
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Random music',
+        artist: 'Jacinto Design'
+    }
+];
+
+//Check if playing
+let isPlaying = false;
+
+
+//Play
+function playSong(){
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause')
+    playBtn.setAttribute('title', 'Pause')
+    music.play();
+}
+
+//Pause
+function pauseSong(){
+    isPlaying = false;
+    if(songIndex < 0){
+        songIndex = songs.length - 1
+    }
+    playBtn.classList.replace('fa-pause', 'fa-play')
+    playBtn.setAttribute('title', 'Play')
+    music.pause();
+}
+
+//Play or pause Event Listener
+playBtn.addEventListener('click', () => {
+    isPlaying ? pauseSong(): playSong()
+});
+
+//Update DOM
+
+function loadSong(song){
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`
+    image.src = `img/${song.name}.jpg`;
+}
+
+//Current song
+
+let songIndex = 0;
+
+//Previous song
+function prevSong(){
+    songIndex--;
+    if(songIndex < 0){
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+};
+
+//Next Song
+function nextSong(){
+    songIndex++;
+    if(songIndex > songs.length - 1){
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+};
+
+loadSong(songs[songIndex]);
+
+//Event Listeners
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
